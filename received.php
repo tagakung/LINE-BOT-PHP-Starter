64681da69b8a468ed45bdd9dc7806397ff1eff0e
Waruth Sivasirikaroon <s.waruth@gmail.com>
2017-06-22 11:34:20 +0700

    Update received.php

--- received.php
+++ received.php
@@ -1,3 +1,22 @@
 <?php
-   echo($_POST['g-recaptcha-response']);
+   $response = $_POST["g-recaptcha-response"];
+	$url = 'https://www.google.com/recaptcha/api/siteverify';
+	$data = array(
+		'secret' => 'YOUR_SECRET',
+		'response' => $_POST["g-recaptcha-response"]
+	);
+	$options = array(
+		'http' => array (
+			'method' => 'POST',
+			'content' => http_build_query($data)
+		)
+	);
+	$context  = stream_context_create($options);
+	$verify = file_get_contents($url, false, $context);
+	$captcha_success=json_decode($verify);
+	if ($captcha_success->success==false) {
+		echo "<p>You are a bot! Go away!</p>";
+	} else if ($captcha_success->success==true) {
+		echo "<p>You are not not a bot!</p>";
+	}
 ?>
