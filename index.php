b955d1ecb753bd6f5ddff947702866134c43fc93
Waruth Sivasirikaroon <s.waruth@gmail.com>
2017-06-22 11:24:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
-<?php
-echo "Hello LINE BOT";
+<html>
+  <head>
+    <title>reCAPTCHA demo: Simple page</title>
+     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+  </head>
+  <body>
+    <form action="?" method="POST">
+      <div class="g-recaptcha" data-sitekey="6LdLbiYUAAAAACjMIc4kms8yQAMV_NDz3j9nKyy-"></div>
+      <br/>
+      <input type="submit" value="Submit">
+    </form>
+  </body>
+</html>
